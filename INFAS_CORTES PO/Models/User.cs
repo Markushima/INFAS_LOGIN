65699c99b1148d9492a0224c7e103680ab29d13a
ACT-STUDENT <jp4419156@gmail.com>
2026-08-17 08:38:39 +0800

    asdasd

--- INFAS_CORTES PO/Models/User.cs
+++ INFAS_CORTES PO/Models/User.cs
@@ -9,45 +9,5 @@
         public string Password { get; set; }
         public string ConfirmPassword { get; set; }
 
-
-
-        //public string _sql(string[] fields, string[] values, string tb)
-        //{
-        //    string fieldList = "";
-        //    string valueList = "";
-
-        //    for (int i = 0; i < fields.Length; i++)
-        //    {
-        //        fieldList += fields[i];
-
-        //        if (i != fields.Length - 1)
-        //        {
-        //            fieldList += ", ";
-        //        }
-        //    }
-
-        //    for (int i = 0; i < values.Length; i++)
-        //    {
-        //        if (int.TryParse(values[i], out _) || double.TryParse(values[i], out _))
-        //        {
-                   
-        //            valueList += values[i];
-        //        }
-        //        else
-        //        {
-                   
-        //            valueList += $"'{values[i]}'";
-        //        }
-
-        //        if (i != values.Length - 1)
-        //        {
-        //            valueList += ", ";
-        //        }
-        //    }
-
-        //    return $"INSERT INTO {tb} ({fieldList}) VALUES ({valueList});";
-        //}
-
-       
     }
 }
